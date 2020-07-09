ff9604448846acf9aecdb3952be3db0ef015021a
sami <sami@dahoux.net>
2020-07-09 16:21:59 +0200

    Writing API documentation

--- cmd_sca/src/csv.h
+++ cmd_sca/src/csv.h
@@ -3,6 +3,12 @@
 
 #include <stdio.h>
 
+/**
+ * @brief Transforms 32-bit registers Hamming weights into a CSV compliant string
+ * @param str result string
+ * @param weights Hamming weights to stringify
+ * @param length of `weights`
+ */
 void CSV_stringify_u32(char* str, unsigned int *weights, size_t len);
 
 #endif // SCA_FRAMEWORK_CSV_H
--- cmd_sca/src/op.h
+++ cmd_sca/src/op.h
@@ -1,6 +1,8 @@
-//
-// Created by Sami Dahoux (sami.dahoux@etu.emse.fr) on 19/06/2020.
-//
+/**
+ * @file op.h
+ * @author Sami Dahoux (s.dahoux@emse.fr)
+ * @brief Module to perform mathematical operations over 32-bit data
+ */
 
 #ifndef SCA_FRAMEWORK_OP_H
 #define SCA_FRAMEWORK_OP_H
@@ -8,10 +10,27 @@
 #include <stdint.h>
 #include <stdio.h>
 
+/**
+ * @brief Computes Hamming weight of the given data
+ * @param data 32-bit raw data
+ * @return value of the weight
+ */
 unsigned int OP_hamming_weight(unsigned int data);
 
+/**
+ * @brief Computes Hamming distance between the two given data
+ * @param left first 32-bit raw data
+ * @param right second 32-bit raw data
+ * @return value of the distance
+ */
 unsigned int OP_hamming_distance(unsigned int left, unsigned int right);
 
+/**
+ * @brief Computes the Hamming weight of the given words
+ * @param words 32-bit values to process
+ * @param weights computed weights
+ * @param len length of `words`
+ */
 void OP_words_to_hamming(const uint32_t *words, unsigned int *weights, size_t len);
 
 #endif //SCA_FRAMEWORK_OP_H
