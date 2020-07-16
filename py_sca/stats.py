b39532c3948d85d74875436c728a46809d4a0773
sami <sami@dahoux.net>
2020-07-16 21:25:54 +0200

    Improving signal filtering

--- py_sca/stats.py
+++ py_sca/stats.py
@@ -8,7 +8,7 @@ def sliding_average(trace, samples):
 
 # noinspection PyTupleAssignmentBalance,PyTypeChecker
 def bandpass_filtering(trace, f_sampling):
-    f_pass = (30e6, 50e6)
+    f_pass = (30e6, 45e6)
     f_stop = (25e6, 55e6)
     g_pass = 3.
     g_stop = 6.
@@ -21,8 +21,8 @@ def bandpass_filtering(trace, f_sampling):
 
 # noinspection PyTupleAssignmentBalance,PyTypeChecker
 def bandpass_filtering_2(trace, f_sampling):
-    f_pass = (1e4, 1e6)
-    f_stop = (0.5e4, 10e6)
+    f_pass = (1e5, 1.3e6)
+    f_stop = (0.5e5, 10e6)
     g_pass = 3.
     g_stop = 10.
 
