d5ae7b99eba4e15d24da4507e2b5501ece747d1a
sami <sami@dahoux.net>
2020-07-21 09:27:07 +0200

    Adding log messages and figure saving

--- py_sca/acquire.py
+++ py_sca/acquire.py
@@ -1,37 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from lib.logger import Log
-
 SEED_NO = 0
-COUNT_TRACES = 32  # count of traces to record from FPGA
-AVG_LEN = 64  # sliding average convolution kernel size
+COUNT_TRACES = 4  # count of traces to record from FPGA
+AVG_LEN = 32  # sliding average convolution kernel size
 F_SAMPLING = 200e6  # sampling frequency of the acquisition system
-TRACES_TO_PLOT = 1  # count of raw traces to plot
-HARDWARE_AES = False
+TRACES_TO_PLOT = 8  # count of raw traces to plot
+HARDWARE_AES = True
 LOG_SOURCE = "serial"
-REPORT_CSV = False
 
-file_args = (COUNT_TRACES, SEED_NO, "_hw" if HARDWARE_AES else "")
+file_args = ("_hw" if HARDWARE_AES else "", COUNT_TRACES)
+
 log = None
+print("*** logging traces ****")
 if LOG_SOURCE == "serial":
     # connect to FPGA via UART and start SCA acquisition
     log = Log.from_serial(COUNT_TRACES, "COM5", hardware=HARDWARE_AES)
 elif LOG_SOURCE == "file":
     # read FPGA acquisition command prompt log file
-    log = Log.from_file("data/cmd_%d_s%d%s.log" % file_args)
+    log = Log.from_file("data/cmd%s_%d.log" % file_args)
 elif LOG_SOURCE == "reports":
     # read FPGA acquisition csv log file
     log = Log.from_reports(
-        "data/report_data_%d_s%d%s.log" % file_args,
-        "data/report_traces_%d_s%d%s.log" % file_args)
+        "data/report_data%s_%d.log" % file_args,
+        "data/report_traces%s_%d.log" % file_args)
 else:
-    raise RuntimeError("Unexpected log source: %s" % LOG_SOURCE)
+    raise RuntimeError("unexpected log source: %s" % LOG_SOURCE)
+
+print("traces successfully logged!")
 
 # log SCA acquisition into CSV reporting files
-if REPORT_CSV:
-    log.report_data("data/report_data.csv")
-    log.report_traces("data/report_traces.csv")
+print("*** exporting traces ****")
+log.report_data("data/acquisition/report_data%s_%d.csv" % file_args)
+log.report_traces("data/acquisition/report_traces%s_%d.csv" % file_args)
+print("export successful!")
 
+print("*** saving traces plots ***")
 # Crop traces and remove offsets
 traces = log.cropped_traces()
 n = len(traces)
@@ -39,10 +43,12 @@ n = len(traces)
 plt.rcParams["figure.figsize"] = (16, 9)
 for idx in range(max(0, n - TRACES_TO_PLOT), n):
     plt.plot(traces[idx])
-    plt.title("Raw power consumption %d (n=%d)" % (idx, n))
-    plt.ylabel("Hamming Weight")
-    plt.xlabel("Time Samples")
-    plt.show()
+
+plt.title("Raw power consumptions (n=%d)" % n)
+plt.xlabel("Time Samples")
+plt.ylabel("Hamming Weights")
+plt.savefig("media/img/acquisition/sca_raw%s_%d" % file_args)
+plt.close()
 
 # Compute the average trace signal and normalize it
 mean = np.array(traces).mean(axis=0)
@@ -52,6 +58,9 @@ plt.plot(mean, color="grey", label="raw data")
 mean = np.convolve(mean, np.ones((AVG_LEN,)) / AVG_LEN, mode="valid")
 plt.plot(mean, color="blue", label="filtered data")
 plt.title("Average power consumption (n=%d)" % n)
-plt.ylabel("Hamming Weight")
 plt.xlabel("Time Samples")
-plt.show()
+plt.ylabel("Hamming Weight")
+plt.savefig("media/img/acquisition/sca_avg%s_%d" % file_args)
+
+print("traces successfully saved!")
+print("exiting...")
