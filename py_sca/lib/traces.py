6e8b4e7c6cc1b66acd4ab0d86e4dccb9fcacedac
sami <sami@dahoux.net>
2020-07-24 21:21:25 +0200

    code: optimized traces sync

--- py_sca/lib/traces.py
+++ py_sca/lib/traces.py
@@ -25,15 +25,25 @@ def sync(traces, step=1, stop=None):
     ref = traces[0]
     n = len(traces)
     m = len(ref)
+    strides_pos = ref.strides * 2
+    strides_neg = (-strides_pos[0], strides_pos[1])
+    shape = (m, m)
     stop = min(stop or m, m)
-    for i in range(n):
-        buffer = np.fromiter(map(
-            lambda shift: stats.pearsonr(ref[:stop], np.roll(traces[i][:stop], shift))[0], list(range(-m, m, step))),
-            dtype=np.float)
-        traces[i] = np.roll(traces[i], np.argmax(buffer) - m)
+    shifts = list(range(0, stop, step))
+    correlate = lambda trace: stats.pearsonr(ref, trace)[0]
+    for i in range(1, n - 1):
+        strided = np.lib.stride_tricks.as_strided(traces[i], shape, strides_pos)
+        buffer = list(map(lambda shift: correlate(strided[shift]), shifts))
+        argmax_pos = np.argmax(buffer)
+        max_pos = buffer[argmax_pos]
+        strided = np.lib.stride_tricks.as_strided(traces[i], shape, strides_neg)
+        buffer = list(map(lambda shift: correlate(strided[shift]), shifts))
+        argmax_neg = np.argmax(buffer)
+        max_neg = buffer[argmax_neg]
+        traces[i] = np.roll(traces[i], -shifts[argmax_pos] if max_neg < max_pos else shifts[argmax_neg])
+
+    shifts = list(range(-stop, stop, step))
+    buffer = list(map(lambda shift: correlate(np.roll(traces[n - 1], shift)), shifts))
+    traces[n - 1] = np.roll(traces[n - 1], np.argmax(buffer) - stop)
 
     return traces
-
-def stride(t, n):
-    strides = t.strides * 2
-    return np.lib.stride_tricks.as_strided(t, (n, n), strides)
