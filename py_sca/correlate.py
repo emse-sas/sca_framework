e9fb63de6c40b388104b1016bfbcb273309c4a47
sami <sami@dahoux.net>
2020-07-25 11:13:30 +0200

    code: byte-1 correlation

--- py_sca/correlate.py
+++ py_sca/correlate.py
@@ -4,10 +4,10 @@ from scipy import stats
 import matplotlib.pyplot as plt
 
 SEED_NO = 0
-COUNT_TRACES = 4  # count of traces to record from FPGA
+COUNT_TRACES = 16384  # count of traces to record from FPGA
 AVG_LEN = COUNT_TRACES  # sliding average convolution kernel size
 F_SAMPLING = 200e6  # sampling frequency of the acquisition system
-TRACES_TO_PLOT = 32  # count of raw traces to plot
+TRACES_TO_PLOT = 64  # count of raw traces to plot
 HARDWARE_AES = True
 
 np.set_printoptions(formatter={'int': hex})
@@ -22,42 +22,51 @@ log = logger.Log.from_reports(
 print(log)
 
 n = len(log.plains)
-handler = aes.Handler(key=aes.words_to_block(log.keys[0]))
 traces = tr.sync(tr.crop(log.traces))
 traces = np.array(traces, dtype=np.float)
 traces -= traces.mean(axis=1).reshape((n, 1))
+
+m = len(traces[0])
 plains = []
 ciphers = []
 keys = []
-hypothesis = []
-m = len(traces[0])
-
-hyp_key = np.zeros((4, 4), dtype=np.ubyte)
+sub_traces = [[{} for j in range(4)] for i in range(4)]
 for d in range(n):
     plains.append(aes.words_to_block(log.plains[d]))
     ciphers.append(aes.words_to_block(log.ciphers[d]))
     keys.append(aes.words_to_block(log.keys[d]))
-    hypothesis.append([])
-    for hyp in range(256):
-        hyp_key[0][0] = hyp
-        handler = aes.Handler(key=hyp_key)
-        handler.encrypt(plains[-1])
-        before = handler.blocks[aes.N_ROUNDS][aes.Stages.START]
-        after = handler.blocks[aes.N_ROUNDS][aes.Stages.SUB_BYTES]
-        hypothesis[-1].append(bin(before[0][0] ^ after[0][0]).count("1"))
+    for i in range(4):
+        for j in range(4):
+            try:
+                sub_traces[i][j][plains[d][i][j]].append(traces[d])
+            except KeyError:
+                sub_traces[i][j][plains[d][i][j]] = [traces[d]]
 
-hypothesis = np.array(hypothesis).T
-traces = traces.T
+for i in range(4):
+    for j in range(4):
+        for k in range(256):
+            try:
+                sub_traces[i][j][k] = np.mean(sub_traces[i][j][k], axis=0) 
+            except KeyError:
+                sub_traces[i][j][k] = np.full((m,), log.offset, dtype=np.float64)
+        sub_traces[i][j] = np.array(list(sub_traces[i][j].values()))
+
+hypothesis = [[0 for k in range(256)] for hyp in range(256)]
+for hyp in range(256):
+    for k in range(256):
+        hypothesis[hyp][k] = bin(aes.S_BOX[k ^ hyp] ^ k).count("1")
+
+m = len(traces[0])
+sub_traces[0][0] = sub_traces[0][0].T
 correlation = np.zeros((256, m))
-for i in range(256):
-    for j in range(m):
-        correlation[i][j] = stats.pearsonr(hypothesis[i], traces[j])[0]
+for hyp in range(256):
+    for t in range(m):
+        correlation[hyp][t] = stats.pearsonr(hypothesis[hyp], sub_traces[0][0][t])[0]
 
-print(hypothesis)
-print(traces)
-
-for i in range(256):
-    plt.plot(correlation[i])
+    if hyp == keys[0][0][0]:
+        plt.plot(correlation[hyp], color="red", zorder=1000)
+    else:
+        plt.plot(correlation[hyp], color="grey")
 
 plt.show()
 print(correlation)
