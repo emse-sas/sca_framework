ba21fe03dd14962ca4a8e4fa3dffd95cd0b65c62
sami <sami@dahoux.net>
2020-08-02 17:36:01 +0200

    code: refactor https://github.com/samiBendou/sca_framework/issues/11
    using os.path and MODE_AES instead of HARDWARE_AES

--- py_sca/acquire.py
+++ py_sca/acquire.py
@@ -2,62 +2,66 @@ import matplotlib.pyplot as plt
 import numpy as np
 from lib import aes, logger
 from lib import traces as tr
+from lib.utils import duration
+import os
 import time
-from datetime import timedelta
 from scipy import fft
 
-COUNT_TRACES = 16 # count of traces to record from FPGA
-AVG_LEN = 512  # sliding average convolution kernel size
+COUNT_TRACES = 16384 # count of traces to record from FPGA
 TRACES_TO_PLOT = 32  # count of raw traces to plot
-HARDWARE_AES = True
+MODE_AES = "hw"
 LOG_SOURCE = "serial"
 SYNC_TRACES = False
 F_SAMPLING = 200e6
 
-file_args = ("_hw" if HARDWARE_AES else "", COUNT_TRACES)
+FILE_ARGS = (MODE_AES, COUNT_TRACES)
+DATA_PATH = os.path.join("data", *["acquisition", MODE_AES])
+IMG_PATH = os.path.join("media", *["img", "acquisition", MODE_AES])
+LOG_PATH = os.path.join("data", "cmd.log")
 
 log = None
 print("*** logging traces ***")
 t_start = time.perf_counter()
 if LOG_SOURCE == "serial":
     # connect to FPGA via UART and start SCA acquisition
-    log = logger.Log.from_serial(COUNT_TRACES, "COM5", hardware=HARDWARE_AES)
+    log = logger.Log.from_serial(COUNT_TRACES, "COM5", hardware=MODE_AES == "hw", baud=460300)
 elif LOG_SOURCE == "file":
     # read FPGA acquisition command prompt log file
-    log = logger.Log.from_file("data/cmd.log")
+    log = logger.Log.from_file(LOG_PATH)
 elif LOG_SOURCE == "reports":
     # read FPGA acquisition csv log file
     log = logger.Log.from_reports(
-        "data/report_data%s_%d.csv" % file_args,
-        "data/report_traces%s_%d.csv" % file_args)
+        os.path.join(DATA_PATH, "report_data_%s_%d.csv" % FILE_ARGS),
+        os.path.join(DATA_PATH, "report_traces_%s_%d.csv" % FILE_ARGS),
+    )
 else:
     raise RuntimeError("unexpected log source: %s" % LOG_SOURCE)
 t_log = time.perf_counter()
-print("traces successfully logged!\nelapsed: %s" % str(timedelta(seconds=t_log - t_start)))
+print("traces successfully logged!\nelapsed: %s" % str(duration(t_log, t_start)))
 
 
 # log SCA acquisition into CSV reporting files
 print("*** exporting traces ***")
 t_start = time.perf_counter()
-log.report_data("data/acquisition/report_data%s_%d.csv" % file_args)
-log.report_traces("data/acquisition/report_traces%s_%d.csv" % file_args)
+log.report_data(os.path.join(DATA_PATH, "report_data_%s_%d.csv" % FILE_ARGS))
+log.report_traces(os.path.join(DATA_PATH, "report_traces_%s_%d.csv" % FILE_ARGS))
 t_export = time.perf_counter()
-print("export successful!\nelapsed: %s" % str(timedelta(seconds=t_export - t_start)))
+print("export successful!\nelapsed: %s" % str(duration(t_export, t_start)))
 
 # Synchronize trace signals and compute average trace
 print("*** processing traces ***")
 t_start = time.perf_counter()
-traces = tr.pad(log.traces)
+traces = tr.pad(log.traces, log.offset)
 if SYNC_TRACES:
     traces = tr.sync(traces, stop=128)
 n, m = traces.shape
 mean = np.array(traces).mean(axis=0)
-smoothed = np.convolve(mean, np.ones((AVG_LEN,)) / AVG_LEN, mode="same")
+smoothed = np.convolve(mean, np.ones((m//11,)) / (m//11), mode="same")
 spectrum = np.absolute(fft.fft(mean - np.mean(mean)))
 freq = np.fft.fftfreq(spectrum.size, 1.0 / F_SAMPLING)[:spectrum.size//2] / 1e6
 f = np.argsort(freq)  
 t_proc = time.perf_counter()
-print("processing successful!\nelapsed: %s" % str(timedelta(seconds=t_proc - t_start)))
+print("processing successful!\nelapsed: %s" % str(duration(t_proc, t_start)))
 
 
 print("*** saving plots ***")
@@ -71,26 +75,29 @@ plt.title("Raw power consumptions (traces: %d, samples: %d, sensors: %d)" % plot
 plt.xlabel("Time Samples")
 plt.ylabel("Hamming Weights")
 plt.legend()
-plt.savefig("media/img/acquisition/sca_raw%s_%d" % file_args)
+plt.tight_layout()
+plt.savefig(os.path.join(IMG_PATH, "sca_raw_%s_%d" % FILE_ARGS))
 plt.close()
 
 plt.plot(mean, color="grey", label="raw data")
-if not HARDWARE_AES:
+if MODE_AES != "hw":
     plt.plot(smoothed, color="blue", label="filtered data")
 plt.title("Average power consumption (traces: %d, samples: %d, sensors: %d)" % plot_args)
 plt.xlabel("Time Samples")
 plt.ylabel("Hamming Weight")
 plt.legend()
-plt.savefig("media/img/acquisition/sca_avg%s_%d" % file_args)
+plt.tight_layout()
+plt.savefig(os.path.join(IMG_PATH, "sca_avg_%s_%d" % FILE_ARGS))
 plt.close()
 
 plt.plot(freq[f], spectrum[f], color="red", label="raw data")
 plt.title("Average power consumption FFT (traces: %d, samples: %d, sensors: %d)" % plot_args)
 plt.xlabel("Frequency (MHz)")
 plt.ylabel("Hamming Weight")
-plt.savefig("media/img/acquisition/sca_avg_fft%s_%d" % file_args)
+plt.tight_layout()
+plt.savefig(os.path.join(IMG_PATH, "sca_avg_fft_%s_%d" % FILE_ARGS))
 plt.close()
 t_plot = time.perf_counter()
-print("traces successfully saved!\nelapsed: %s" % str(timedelta(seconds=t_plot - t_start)))
+print("traces successfully saved!\nelapsed: %s" % str(duration(t_plot, t_start)))
 
 print("exiting...")
--- py_sca/correlate.py
+++ py_sca/correlate.py
@@ -1,27 +1,33 @@
 from lib import aes, logger, cpa
 from lib import traces as tr
+from lib.utils import duration
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 import time
 from datetime import timedelta
+import os
+from itertools import product
 
 COUNT_TRACES = 16384 # count of traces to record from FPGA
-HARDWARE_AES = True
+MODE_AES = "hw"
 SYNC_TRACES = False
+
+FILE_ARGS = (MODE_AES, COUNT_TRACES)
+DATA_PATH = os.path.join("data", *["acquisition", MODE_AES])
+IMG_PATH = os.path.join("media", *["img", "correlation", MODE_AES])
 
 np.set_printoptions(formatter={'int': hex})
 
-file_args = ("_hw" if HARDWARE_AES else "", COUNT_TRACES)
 
 # read FPGA acquisition csv log file
 print("*** importing traces ***")
 t_start = time.perf_counter()
 log = logger.Log.from_reports(
-    "data/acquisition/report_data%s_%d.csv" % file_args,
-    "data/acquisition/report_traces%s_%d.csv" % file_args)
+    os.path.join(DATA_PATH, "report_data_%s_%d.csv" % FILE_ARGS),
+    os.path.join(DATA_PATH, "report_traces_%s_%d.csv" % FILE_ARGS))
 t_import = time.perf_counter()
-print("import successful!\nelapsed: %s" % str(timedelta(seconds=t_import - t_start)))
+print("import successful!\nelapsed: %s" % str(duration(t_import, t_start)))
 
 print("*** processing traces ***")
 t_start = time.perf_counter()
@@ -33,41 +39,41 @@ traces = np.array(traces, dtype=np.float64)
 traces -= traces.mean(axis=1).reshape((n, 1))
 m = len(traces[0])
 t_proc = time.perf_counter()
-print("processing successful!\nelapsed: %s" % str(timedelta(seconds=t_proc - t_start)))
+print("processing successful!\nelapsed: %s" % str(duration(t_proc, t_start)))
 
 print("*** creating handler ***")
 t_start = time.perf_counter()
 handler = cpa.Handler.from_log(log, traces)
 t_handler = time.perf_counter()
-print("handler successfully created!\nelapsed: %s" % str(timedelta(seconds=t_handler - t_start)))
+print("handler successfully created!\nelapsed: %s" % str(duration(t_handler, t_start)))
 
 print("*** computing correlation ***")
 t_start = time.perf_counter()
 correlations = handler.correlations()
 t_cor = time.perf_counter()
-print("correlation successfully computed!\nelapsed: %s" % str(timedelta(seconds=t_cor - t_start)))
+print("correlation successfully computed!\nelapsed: %s" % str(duration(t_cor, t_start)))
 
 print("*** saving plots ***")
 t_start = time.perf_counter()
 plt.rcParams["figure.figsize"] = (16, 9)
-for i in range(aes.BLOCK_LEN):
-    for j in range(aes.BLOCK_LEN):
-        for h in range(cpa.COUNT_HYP):
-            if h == handler.key[i, j]:
-                plt.plot(correlations[i, j, h], color="red", label="key 0x%x" % h, zorder=1000)
-            else:
-                plt.plot(correlations[i, j, h], color="grey")
-        
-        img_args = tuple([i * aes.BLOCK_LEN + j]) + file_args
-        plt.title("Time Correlation byte %d (n=%d, m=%d)" % (img_args[0], n, m))
-        plt.xlabel("Time Samples")
-        plt.ylabel("Pearson Correlation")
-        plt.legend()
-        plt.savefig("media/img/correlation/b%d/sca_cor%s_%d" % img_args)
-        plt.close()
+for i, j in product(range(aes.BLOCK_LEN), range(aes.BLOCK_LEN)):
+    for h in range(cpa.COUNT_HYP):
+        if h == handler.key[i, j]:
+            plt.plot(correlations[i, j, h], color="red", label="key 0x%x" % h, zorder=1000)
+        else:
+            plt.plot(correlations[i, j, h], color="grey")
+    
+    img_args = tuple([i * aes.BLOCK_LEN + j]) + FILE_ARGS
+    plt.title("Time Correlation byte %d (n=%d, m=%d)" % (img_args[0], n, m))
+    plt.xlabel("Time Samples")
+    plt.ylabel("Pearson Correlation")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(os.path.join(IMG_PATH, "sca_cor_b%d_%s_%d" % img_args))
+    plt.close()
 
 t_plot = time.perf_counter()
-print("traces successfully saved!\nelapsed: %s" % str(timedelta(seconds=t_plot - t_start)))
+print("traces successfully saved!\nelapsed: %s" % str(duration(t_plot, t_start)))
 
 print("exiting...")
 
