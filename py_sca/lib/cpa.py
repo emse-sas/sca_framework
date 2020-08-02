f7e2dba3cb097e5eec4a1f18bc8196046677b52e
sami <sami@dahoux.net>
2020-08-02 17:33:08 +0200

    code: loop refactor with itertools

--- py_sca/lib/cpa.py
+++ py_sca/lib/cpa.py
@@ -3,6 +3,7 @@ import concurrent.futures
 import numpy as np
 from scipy import stats
 import threading
+from itertools import product
 
 
 COUNT_HYP = 256
@@ -29,48 +30,45 @@ class Handler:
         blocks = np.empty((n, aes.BLOCK_LEN, aes.BLOCK_LEN), dtype=np.ubyte)
         for d in range(n):
             blocks[d] = aes.words_to_block(log_blocks[d])
-            for i in range(aes.BLOCK_LEN):
-                for j in range(aes.BLOCK_LEN):
-                    try:
-                        sort[i][j][blocks[d, i, j]].append(traces[d])
-                    except KeyError:
-                        sort[i][j][blocks[d, i, j]] = [traces[d]]
+            for i, j in product(range(aes.BLOCK_LEN), range(aes.BLOCK_LEN)):
+                try:
+                    sort[i][j][blocks[d, i, j]].append(traces[d])
+                except KeyError:
+                    sort[i][j][blocks[d, i, j]] = [traces[d]]
  
         m = len(traces[0])
         lens = np.zeros((aes.BLOCK_LEN, aes.BLOCK_LEN, COUNT_CLS), dtype=np.int)
         means = np.zeros((aes.BLOCK_LEN, aes.BLOCK_LEN, COUNT_CLS, m), dtype=np.float64)
         devs = np.zeros((aes.BLOCK_LEN, aes.BLOCK_LEN, COUNT_CLS, m), dtype=np.float64) 
-        for i in range(aes.BLOCK_LEN):
-            for j in range(aes.BLOCK_LEN):
-                for k in sort[i][j].keys():
-                    lens[i, j, k] = len(sort[i][j][k])
-                    means[i, j, k] = np.mean(sort[i][j][k], axis=0)
-                    devs[i, j, k] = np.std(sort[i][j][k], axis=0)
+        for i, j, k in product(range(aes.BLOCK_LEN), range(aes.BLOCK_LEN), range(COUNT_CLS)):
+            try:
+                lens[i, j, k] = len(sort[i][j][k])
+            except KeyError:
+                continue
+            means[i, j, k] = np.mean(sort[i][j][k], axis=0)
+            devs[i, j, k] = np.std(sort[i][j][k], axis=0)
 
         mean = np.mean(traces, axis=0)
         dev = np.std(traces, axis=0)
         return Handler(blocks, aes.words_to_block(log.keys[0]), lens, mean, means, dev, devs)
 
     def init_model(self):
-        for h in range(COUNT_HYP):
-            for k in range(COUNT_CLS):
-                self.hyp[h, k] = bin(aes.S_BOX[k ^ h] ^ k).count("1")
+        for h, k in product(range(COUNT_HYP), range(COUNT_CLS)):
+            self.hyp[h, k] = bin(aes.S_BOX[k ^ h] ^ k).count("1")
 
     def correlations(self):
         m = len(self.means[0, 0, 0])
         ret = np.empty((aes.BLOCK_LEN, aes.BLOCK_LEN, COUNT_HYP, m), dtype=np.float64)
 
-        for i in range(aes.BLOCK_LEN):
-            for j in range(aes.BLOCK_LEN):
-                self._pearson_fast(i, j, m, ret)
+        for i, j in product(range(aes.BLOCK_LEN), range(aes.BLOCK_LEN)):
+            self._pearson_fast(i, j, m, ret)
 
         return ret
 
     def _pearson(self, i, j, m, ret):
         means = self.means[i, j].T
-        for h in range(COUNT_HYP):
-            for t in range(m):
-                ret[i, j, h, t] = stats.pearsonr(self.hyp[h], means[t])[0]
+        for h, t in product(range(COUNT_HYP), range(m)):
+            ret[i, j, h, t] = stats.pearsonr(self.hyp[h], means[t])[0]
 
     def _pearson_fast(self, i, j, m, ret):
         for h in range(COUNT_HYP):
