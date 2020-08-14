d1579dde5d994cce17c670729cbc4383fe7e7e63
sami <sami@dahoux.net>
2020-08-14 14:49:24 +0200

    code: calibration and start message in main #35

--- cmd_sca/src/main.c
+++ cmd_sca/src/main.c
@@ -1,7 +1,11 @@
 #include "run.h"
+#include "tdc_hw.h"
+#define SCA_PROJECT_VERSION "1.1.0"
 
 int main(int argc, char *argv[])
 {
-	RUN_cmd();
+    printf("\n\n*** cmd_sca %s ***\n\n", SCA_PROJECT_VERSION);
+    TDC_HW_calibrate(0);
+    RUN_cmd();
     return 0;
 }
