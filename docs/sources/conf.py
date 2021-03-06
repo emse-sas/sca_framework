54b5b7fba297aac6247689082687a8a2669fc0bb
joseph gravellier <joseph.gravellier@emse.fr>
2021-03-06 23:33:24 +0100

    test remote literalinclude

--- docs/sources/conf.py
+++ docs/sources/conf.py
@@ -37,6 +37,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinxarg.ext",
     "recommonmark",
+    "sphinxext.remoteliteralinclude"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,10 +56,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
     #
 html_theme = 'sphinx_rtd_theme'
 
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = []
 
 autodoc_member_order = 'bysource'
